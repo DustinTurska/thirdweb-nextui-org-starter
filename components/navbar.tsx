18dee7223ebd8ad7c2545a8c61e04f0efc0665d0
Dustin <135719141+DustinTurska@users.noreply.github.com>
2024-10-26 21:18:22 -0500

    Remove theme-switch.tsx
    Remove ThemeSwitch

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,7 +16,6 @@ import NextLink from "next/link";
 import clsx from "clsx";
 
 import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
 import {
   TwitterIcon,
   GithubIcon,
@@ -91,7 +90,6 @@ export const Navbar = () => {
           <Link isExternal aria-label="Github" href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
           </Link>
-          <ThemeSwitch />
         </NavbarItem>
         <NavbarItem className="hidden md:flex">
           <ConnectButton client={client} />
@@ -102,7 +100,6 @@ export const Navbar = () => {
         <Link isExternal aria-label="Github" href={siteConfig.links.github}>
           <GithubIcon className="text-default-500" />
         </Link>
-        <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
 
